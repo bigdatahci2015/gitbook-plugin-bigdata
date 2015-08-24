5ce4c3501ba79a034d3e5726bd6c394a0efe5e73
Tom Yeh <doubleshow@gmail.com>
2015-08-23 20:37:44 -0600

    new blocks

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ module.exports = {
                         + blk.body  +
                     '</svg>'
                 }
-            }
+            },
+            lodash: {
+                process: function(blk){
+                    // console.log(blk.body)
+
+                    var f = new Function('data', '_', blk.body)
+
+                    var ret = f.call(this, this.ctx.data, lodash)
+                    console.log(ret)
+                    this.ctx.result = ret
+                    // console.log(this.ctx.data.comments)
+                    return '<pre>' + blk.body +'</pre>'
+                }
+            },
+            json: {
+                process: function(blk){
+                    return '<pre>' + blk.body + '</pre>'
+                }
+            }            
     }
 }
