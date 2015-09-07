57ad98c7d47cfd03affb8697e07b25c683441136
Tom Yeh <doubleshow@gmail.com>
2015-09-07 13:46:18 -0600

    lodash exercise block

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var lodash = require('lodash')
 var hl = require('highlight').Highlight
+var _ = require('lodash')
+var chai = require('chai')
+var assert = require('chai').assert
+var jade = require('jade')
 
 module.exports = {
     website: {
@@ -62,6 +66,65 @@ module.exports = {
                         html = html + '<div class="console error">' + error.stack + '</div>'
                     return html
 
+                }
+            },
+            lodashexercise: {
+                blocks: ["data","output","solution","title"],
+                process: function(blk) {
+
+                    var codes = {}
+                    _.forEach(blk.blocks, function(blk){
+                        codes[blk.name] = _.get(blk, 'body', '').trim()
+                    })
+
+                    var txt = ''
+                    var myconsole = {
+                        log: function(o){
+                            // console.log(o)
+                            txt = txt + 'console> ' + JSON.stringify(o)
+                            txt += '\n'
+                        },
+                        debug: function(o){
+                            // console.debug(o)
+                            txt = txt + 'debug> ' + JSON.stringify(o)
+                            txt += '\n'
+                        }
+                    }
+
+                    var ctx = {}
+                    var locals = {
+                        ctx: ctx,
+                        codes: codes,
+                        error: null,
+                        ret: null,
+                        hl: hl,
+                        txt: txt
+                    }
+
+                    var error
+                    try {
+
+                        var dataFunc = new Function('ctx', 'ctx.data = ' + codes.data)
+                        var outputFunc = new Function('ctx', 'ctx.output = ' + codes.output)
+                        dataFunc.call(this, ctx)
+                        outputFunc.call(this, ctx)
+
+                        // try {
+                        var solutionFunc = new Function('data', '_', 'console', codes.solution)
+                        var ret = solutionFunc.call(this, ctx.data, lodash, myconsole)
+                        locals.ret = ret
+                        locals.match = _.isEqual(ctx.output, ret)
+                        locals.txt = txt
+                        // } catch (err){
+                        //     // locals.
+                        // }
+
+                    } catch (err) {
+                        console.error(err)
+                        locals.error = err
+                    }
+                    // console.log(locals)
+                    return jade.renderFile(__dirname + '/lodashexercise.jade', locals)
                 }
             },
             json: {
